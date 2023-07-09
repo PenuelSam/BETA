6c521d0a9ba5bcb446aa09d4b7138e979383188d
PenuelSam <penuelsamuel54@gmail.com>
2023-07-09 17:36:35 +0100

    commit

--- src/Components/careHome2/Banner.jsx
+++ src/Components/careHome2/Banner.jsx
@@ -1,12 +1,14 @@
 
 const Banner = () => {
   return (
-    <div className="care-h-BannerBg pt-[15%] lg:pt-[40%] sm:pt-[60%]">
+    <div className="care-h-BannerBg pt-[20%] sm:pt-[15%]">
         <div className="maxcontainer2 care-h-container">
-            <span className="care-h-span">Welcome to</span>
-            <h1 className="care-h-H1">BETA CARE</h1>
-            <h2 className="care-h-H2">The Best Home Care Out there.</h2>
-            <p className="care-h-P">BetaCare offers quality services to individuals in need of care. We have services are in varying dimensions which includes Home, Live-in, Overnight, Respite, End-of-life care and many more.</p>
+            <span className="care-h-span sm:care-h-spansm ">Welcome to</span>
+            <div className="sm:pt-[40%] flex flex-col justify-center items-end">
+            <h1 className="care-h-H1 sm:care-h-H1sm">BETA CARE</h1>
+            <h2 className="care-h-H2 sm:care-h-H2sm">The Best Home Care Out there.</h2>
+            <p className="care-h-P sm:care-h-Psm">BetaCare offers quality services to individuals in need of care. We have services are in varying dimensions which includes Home, Live-in, Overnight, Respite, End-of-life care and many more.</p>
+            </div>
         </div>
     </div>
   )
