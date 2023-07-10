a64dd4b5e4f1662a84e3e5210016ac0a5c2d3800
PenuelSam <penuelsamuel54@gmail.com>
2023-07-10 10:09:29 +0100

    commit

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -15,9 +15,9 @@ const Card2 = ({Title, Paragraph, Image}) => {
 
   return (
     <div  className="w-[100%]">
-        <div data-aos="zoom-in" data-aos-duration="800" data-aos-delay="200" className="flex justify-center sm:my-5 my-10"><h1 className="cardTitle" >{Title}</h1></div>
-        <div className="flex flex-row-reverse lg:flex-col lg:items-start items-center gap-[11rem] sm:gap-5">
-            <div data-aos="zoom-in" data-aos-duration="800" data-aos-delay="200" className="w-[729px] sm:w-[100%] sm:h-[400px] h-[729px] relative lg:left-0 sm:left-0 xsm:left-0 sm:px-5 right-[-50px] sm:cardImageCenter z-[10]"><img src={Image} alt="" /></div>
+        <div data-aos="zoom-in" data-aos-duration="800" data-aos-delay="200" className="flex justify-center  my-10"><h1 className="cardTitle" >{Title}</h1></div>
+        <div className="flex flex-row-reverse lg:flex-col lg:items-start items-center gap-[11rem] sm:gap-20 xsm:gap-[50px] ">
+            <div data-aos="zoom-in" data-aos-duration="800" data-aos-delay="200" className="w-[729px] sm:w-[100%] sm:h-[300px] h-[729px] relative lg:left-0 sm:left-0 xsm:left-0 sm:px-5 right-[-50px] sm:cardImageCenter z-[10]"><img src={Image} alt="" /></div>
             <div data-aos="zoom-in"  data-aos-duration="800" data-aos-delay="200"><p className="card2Paragraph">{Paragraph}</p></div>
         </div>
     </div>
