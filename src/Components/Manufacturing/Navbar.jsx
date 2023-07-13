30a3977ec9ccf41a56166474925e5ab976549672
PenuelSam <penuelsamuel54@gmail.com>
2023-07-13 18:39:08 +0100

    commit

--- src/Components/Manufacturing/Navbar.jsx
+++ src/Components/Manufacturing/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = () => {
                     </div>
                     </div>                  
                   </Link>
-                  <div className=" ml-[8%] w-[230px] h-[0.1px] bg-[#686868]"></div> 
+                  <div className=" ml-[8%] w-[230px] h-[0.1px] bg-[#dddcdc]"></div> 
                 </>
               ))}
             </ul>
