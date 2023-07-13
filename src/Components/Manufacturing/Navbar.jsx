d41622ef57799d05305660b292d8385422d75a59
PenuelSam <penuelsamuel54@gmail.com>
2023-07-13 15:36:28 +0100

    commit

--- src/Components/Manufacturing/Navbar.jsx
+++ src/Components/Manufacturing/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               onClick={() => setToggleMenu(false)}
             />
             <Link to="/" onClick={() => handleClick('/')}>
-              <img src={logo2} alt="" className="w-[200px] relative top-[-25%] " />
+              <img src={logo2} alt="" className="w-[200px] relative top-[-28%] " />
             </Link>
             <ul className="navbar-smallscreen_links">
             {NavLinks.map((nav) => (
