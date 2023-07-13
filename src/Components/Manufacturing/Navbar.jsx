8c03f578106f24a880de4cdf55f71108ab117612
PenuelSam <penuelsamuel54@gmail.com>
2023-07-13 16:53:24 +0100

    commit

--- src/Components/Manufacturing/Navbar.jsx
+++ src/Components/Manufacturing/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
                         {nav.name}
                       </li>
                       </div>
-                      <ArrowRightOutlinedIcon sx={{fill: '#434445'}} className="absolute right-0" />
+                      <ArrowRightOutlinedIcon sx={{fill: '#434445'}} className="absolute right-[-10%]" />
                     </div>
                     </div>                  
                   </Link>
